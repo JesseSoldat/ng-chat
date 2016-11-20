a4d8afe12e14052e4c8507a6f7f4476db53a0744
jesse soldat <jsoldat@hotmail.com>
2016-11-20 10:39:05 +0700

    replace starter template

---
+++ app.ts
@@ -0,0 +1,18 @@
+import {Component} from 'angular2/core';
+import {bootstrap} from 'angular2/platform/browser';
+
+
+@Component({
+	selector: 'chat-app',
+	template: `
+	<div>
+		Chat
+	</div>
+	`
+})
+
+class ChatApp {
+	
+}
+
+bootstrap(ChatApp);
